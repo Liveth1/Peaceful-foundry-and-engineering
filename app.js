47818563b9bb33f6951f4552008cad12d711208a
Liveth1 <163150687+Liveth1@users.noreply.github.com>
2024-09-30 11:08:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+function test() {
+  return new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+}
+
+function trigger() {
+  const swiper = test();
+  swiper.init();
+}
+
+setTimeout(trigger, 3000);
